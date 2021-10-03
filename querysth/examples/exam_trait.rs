0b700c10fc8576973d8f810a82ed4c105bc293c9
hanxuanliang <hxuanliang@163.com>
2021-10-03 09:17:14 +0800

    update with trait constraint

--- querysth/examples/exam_trait.rs
+++ querysth/examples/exam_trait.rs
@@ -1,16 +1,20 @@
+use std::str::FromStr;
 use regex::Regex;
 
 pub trait Parse {
     fn parse(s: &str) -> Self;
 }
 
-impl Parse for u8 {
+impl<T> Parse for T where T: Defult + FromStr {
     fn parse(s: &str) -> Self {
-        let re = Regex::new(r"^[0-9]+").unwrap();
+        let re = Regex::new(r"^[0-9]+(\.[0-9]+)").unwrap();
+        let d = || Default::default();
         if let Some(cap) = re.captures(s) {
-            cap.get(0).map_or(0, |s| s.as_str().parse().unwrap_or(0))
+            cap
+                .get(0)
+                .map_or(0, |s| s.as_str().parse().unwrap_or(0))
         } else {
-            0
+            d()
         }
     }
 }
