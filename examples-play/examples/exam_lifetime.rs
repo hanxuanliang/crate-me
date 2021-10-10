5e12ed8e3e972c653b57bb6ffa2dab8af22c5c71
hanhotfox <hxuanliang@163.com>
2021-10-10 14:08:05 +0800

    example

--- examples-play/examples/exam_lifetime.rs
+++ examples-play/examples/exam_lifetime.rs
@@ -1,16 +1,19 @@
-fn main() {
-    let s1 = String::from("Lindsey");
-    let s2 = String::from("Rosie");
-
-    let result = max(&s1, &s2);
-
-    println!("bigger one: {}", result);
+fn strtok<'a>(s: &mut &'a str, d: char) -> &'a str {
+    if let Some(i) = s.find(d) {
+        let prefix = &s[..i];
+        let suffix = &s[(i + d.len_utf8())..];
+        *s = suffix;
+        prefix
+    } else {
+        let prefix = *s;
+        *s = "";
+        prefix
+    }
 }
 
-fn max<'a>(s1: &'a str, s2: &'a str) -> &'a str {
-    if s1 > s2 {
-        s1
-    } else {
-        s2
-    }
+fn main() {
+    let s = "hello world".to_string();
+    let mut s1 = s.as_str();
+    let hello = strtok(&mut s1, ' ');
+    println!("hello is {}, s1 is {}, s is {}", hello, s1, s)
 }
