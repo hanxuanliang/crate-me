13b0268c2462690a10d1061e010d740949f4ec3b
hanxuanliang <hxuanliang@163.com>
2021-10-25 14:35:58 +0800

    update match_with

--- mgrep/src/lib.rs
+++ mgrep/src/lib.rs
@@ -1,8 +1,11 @@
+use std::{fs::File, io, io::{BufReader, Stdout}, path::Path};
 use anyhow::Result;
 use clap::Parser;
 
 mod errors;
 pub use errors::GrepError;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+use regex::Regex;
 
 #[derive(Parser, Debug)]
 #[clap(version = "1.0")]
@@ -11,12 +14,25 @@ pub struct GrepConfig {
     glob: String,
 }
 
-impl GrepConfig {
-    fn match_default_strategy(&self) -> Result<(), GrepError> {
-        self.match_with()
-    }
+type StrategyFn<W, R> = fn(&Path, BufReader<R>, &Regex, &mut W) -> Result<(), GrepError>;
 
-    fn match_with(&self) -> Result<(), GrepError> {
+impl GrepConfig {
+    fn match_with(&self, strategy: StrategyFn<Stdout, File>) -> Result<(), GrepError> {
+        let regex = Regex::new(&self.pattern)?;
+        let files: Vec<_> = glob::glob(&self.glob)?.collect();
+
+        files.into_par_iter().for_each(|v| {
+            if let Ok(filename) = v {
+                if let Ok(file) = File::open(&filename) {
+                    let read = BufReader::new(file);
+                    let mut stdout = io::stdout();
+                    
+                    if let Err(e) = strategy(filename.as_path(), read, &regex, &mut stdout) {
+                        println!("Internal Error: {:?}", e);
+                    }
+                }
+            }
+        });
         Ok(())
     }
 }
